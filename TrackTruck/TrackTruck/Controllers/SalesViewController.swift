6c629689690e18bc7f204aaab78a4e3a67c0fec3
Diego Linares <u201410419@upc.edu.pe>
2019-06-14 17:01:31 -0400

    Implementaciòn de Vista de Owner, Vista de Sales Incompleta

---
+++ TrackTruck/TrackTruck/Controllers/SalesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SalesViewController.swift
+//  TrackTruck
+//
+//  Created by user917928 on 6/14/19.
+//  Copyright © 2019 pe.edu.upc. All rights reserved.
+//
+
+import UIKit
+
+class SalesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TrackTruck/TrackTruck/View/SaleCell.swift
@@ -0,0 +1,29 @@
+//
+//  SaleCell.swift
+//  TrackTruck
+//
+//  Created by user917928 on 6/14/19.
+//  Copyright © 2019 pe.edu.upc. All rights reserved.
+//
+
+import UIKit
+
+class SaleCell: UITableViewCell {
+
+    @IBOutlet weak var registerLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func configureCell(sale: Sale){
+        registerLabel.text = sale.employeeName
+        dateLabel.text = sale.date
+        contentLabel.text = sale.content
+        amountLabel.text = String(sale.value)
+    }
+
+}
